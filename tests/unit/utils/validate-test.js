1571b999c207f7034020fa79d8c2505339dfc640
Mihai Scurtu <mscurtu90@gmail.com>
2015-04-10 17:40:00 +0300

    Add tests for validating ember object (which would be the most used case :/)

--- tests/unit/utils/validate-test.js
+++ tests/unit/utils/validate-test.js
@@ -1,6 +1,7 @@
 import validate from '../../../utils/validate';
 import { module, test } from 'qunit';
 
+import Ember from 'ember';
 import Required from 'ember-simple-validate/lib/validators/required';
 
 var validators;
@@ -40,4 +41,32 @@ test('errors are retrievable', function(assert) {
   var result = validate(user, validators);
 
   assert.ok(validators.lastName.errors[0]);
+});
+
+test('it works with ember objects', function(assert) {
+  var userObject = Ember.Object.create(user);
+
+  var result = validate(userObject, validators);
+
+  assert.equal(result, true);
+});
+
+test('it works with computed properties', function(assert) {
+  var UserClass = Ember.Object.extend({
+    firstName: 'Mihai',
+    lastName: 'Scurtu',
+
+    fullName: Ember.computed('firstName', 'lastName', function(key, value) {
+      return Ember.get(this, 'firstName') + ' ' + Ember.get(this, 'lastName');
+    }),
+  });
+
+  var userObject = UserClass.create();
+
+  validators = {
+    fullName: Required.create(),
+  };
+
+  var result = validate(userObject, validators);
+  assert.equal(result, true);
 });
