1b48a53d95ed3577da40cc6d2d62721e4a3c1c38
Mihai Scurtu <mscurtu90@gmail.com>
2015-04-14 10:56:51 +0300

    Create basic mixin functionality

--- addon/mixins/simple-validation-mixin.js
+++ addon/mixins/simple-validation-mixin.js
@@ -1,4 +1,53 @@
 import Ember from 'ember';
 
+import _validate from 'ember-simple-validate/utils/validate';
+import Validator from 'ember-simple-validate/lib/validator';
+
+var get = Ember.get;
+var set = Ember.set;
+
 export default Ember.Mixin.create({
+  init: function() {
+    set(this, 'validators', {});
+    set(this, 'validationErrors', {});
+    set(this, 'isValid', undefined);
+  },
+
+  validate: function() {
+    if(!this.canValidate()) {
+      set(this, 'isValid', true);
+    } else {
+      var self = this;
+
+      var validators = get(this, 'validators');
+
+      set(this, 'isValid', _validate(this, validators));
+
+      Ember.keys(validators).forEach(function(key) {
+        set(self, 'validationErrors.' + key, get(validators[key], 'errors'));
+      });
+    }
+
+    return this.get('isValid');
+  },
+
+  canValidate: function() {
+    var validators = get(this, 'validators');
+    var canValidate = true;
+
+    var keys = Ember.keys(validators);
+
+    if(keys && keys.length) {
+      keys.forEach(function(key) {
+          if(!validators[key] instanceof Validator) {
+            canValidate = false;
+            return false;
+          }
+      });
+    } else {
+      canValidate = false;
+    }
+
+    return canValidate;
+  }
 });
--- tests/unit/mixins/simple-validation-mixin-test.js
+++ tests/unit/mixins/simple-validation-mixin-test.js
@@ -1,12 +1,56 @@
 import Ember from 'ember';
-import SimpleValidationMixinMixin from 'ember-simple-validate/mixins/simple-validation-mixin';
+import SimpleValidationMixin from 'ember-simple-validate/mixins/simple-validation-mixin';
 import { module, test } from 'qunit';
 
-module('SimpleValidationMixinMixin');
+import ValidatorCollection from 'ember-simple-validate/services/validator-collection';
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  var SimpleValidationMixinObject = Ember.Object.extend(SimpleValidationMixinMixin);
-  var subject = SimpleValidationMixinObject.create();
-  assert.ok(subject);
+var UserClass = Ember.Object.extend(SimpleValidationMixin, {
+  firstName: 'Mihai',
+  lastName: 'Scurtu',
+});
+
+var user;
+var Assert;
+var validators;
+
+module('SimpleValidationMixin', {
+  beforeEach: function() {
+    user = UserClass.create();
+
+    Assert = ValidatorCollection.create();
+
+    validators = {
+      firstName: Assert.required(),
+      lastName: Assert.required(),
+    };
+  }
+});
+
+test('it validates a simple object', function(assert) {
+  user.set('validators', validators);
+
+  user.validate();
+
+  assert.equal(user.get('isValid'), true);
+});
+
+test('it catches errors', function(assert) {
+  user.set('validators', validators);
+  user.set('firstName', '');
+
+  assert.equal(user.get('validationErrors.firstName'), undefined);
+
+  user.validate();
+
+  assert.equal(user.get('isValid'), false);
+  assert.equal(user.get('validationErrors.firstName').length, 1);
+});
+
+test("doesn't validate without validators", function(assert) {
+  assert.equal(user.canValidate(), false);
+});
+
+test('it correctly sets canValidate', function(assert) {
+  user.set('validators', validators);
+  assert.equal(user.canValidate(), true);
 });
